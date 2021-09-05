1118d0176082cf435b21bf27777ba45ea3c1d8d1
Mati <matiurrehman.meersayed@gmail.com>
2021-09-06 01:21:25 +0530

    update : Featured Projects

--- src/components/projects/featured-projects/featuredProjectsInfo.js
+++ src/components/projects/featured-projects/featuredProjectsInfo.js
@@ -1,6 +1,7 @@
 import Marketplace from "assets/featured-projects-images/farmerMarketPlace.png";
 import EmojiInterpreter from "assets/featured-projects-images/emojiInterPreter.png";
-
+import highJobAlert from "assets/featured-projects-images/highJobAlert.png";
+import floatingButton from "assets/featured-projects-images/floatingButton.jpg"
 const featuredProjectsList = [
   {
     id: 1,
@@ -43,6 +44,48 @@ const featuredProjectsList = [
     externalLink: "https://mati018.github.io/emoji_interpreter/",
     githubLink: "https://github.com/Mati018/emoji_interpreter",
     githubAvailability: "yes",
+  },
+  {
+    id: 3,
+    projectName: "High Jobalert",
+    imageSrc: highJobAlert,
+    summary:
+      "High Jobalert project is web application built using PHP, MySQL as backend and HTML JavaScript & Bootstrap as Frontend technologies.",
+    tags: [
+      {
+        tagName: "HTML",
+      },
+      {
+        tagName: "CSS",
+      },
+      {
+        tagName: "PHP",
+      },
+      {
+        tagName: "MySQL",
+      }
+    ],
+    externalLink: "http://highjobalert.com/",
+    githubLink: "https://github.com/Mati018/High-Jobalert",
+    githubAvailability: "yes",
+  },
+  {
+    id: 4,
+    projectName: "Floating Button",
+    imageSrc: floatingButton,
+    summary:
+      "Floating Volume is a simple app that allows you to control media, ringer, alarm and in-call volume, and switch between ringer modes within the floating interface.",
+    tags: [
+      {
+        tagName: "Java",
+      },
+      {
+        tagName: "HTML",
+      }
+    ],
+    externalLink: "https://www.amazon.com/gp/product/B089HW8PDW",
+    githubLink: "https://github.com/Mati018",
+    githubAvailability: "no",
   }
 ];
 
